331158d93c44fa57e7fbeefcb76f2f92779e2ef1
giovannasouzv <giovana11vs@gmail.com>
2025-06-08 18:25:00 -0300

    Update tubo.cpp

--- src/tubo.cpp
+++ src/tubo.cpp
@@ -2,6 +2,7 @@
 #include "constants.h"
 #include <allegro5/allegro_image.h>
 #include <cstdlib>
+#include <cstdio>
 
 ALLEGRO_BITMAP* Tubo::imagem_cima = nullptr;
 ALLEGRO_BITMAP* Tubo::imagem_baixo = nullptr;
@@ -17,28 +18,37 @@ void destruir_imagens_tubo() {
 }
 
 Tubo::Tubo(int posicao_x) {
-    x = posicao_x;
-    altura_abertura = rand() % (ALTURA_TELA - ESPACO_ENTRE_TUBOS - 100) + 50;
+    this->x = posicao_x;
+    this->altura_abertura = rand() % (ALTURA_TELA - ESPACO_ENTRE_TUBOS - 150) + 75;
 }
 
 void Tubo::atualizar() {
-    x -= VELOCIDADE_TUBO;
-    if (x + LARGURA_TUBO < 0) {
-        x = LARGURA_TELA;
-        altura_abertura = rand() % (ALTURA_TELA - ESPACO_ENTRE_TUBOS - 100) + 50;
+    this->x -= VELOCIDADE_TUBO;
+    if (this->x + LARGURA_TUBO < 0) {
+        this->x = LARGURA_TELA;
+        this->altura_abertura = rand() % (ALTURA_TELA - ESPACO_ENTRE_TUBOS - 150) + 75;
     }
 }
 
 void Tubo::desenhar() {
-    al_draw_bitmap(imagem_cima, x, altura_abertura - ALTURA_TUBO, 0);
-    al_draw_bitmap(imagem_baixo, x, altura_abertura + ESPACO_ENTRE_TUBOS, 0);
+        al_draw_bitmap(imagem_cima, this->x, this->altura_abertura - ALTURA_TUBO, 0);
+        al_draw_bitmap(imagem_baixo, this->x, this->altura_abertura + ESPACO_ENTRE_TUBOS, 0);
 }
 
+
 bool Tubo::colide(int jogador_x, int jogador_y, int jogador_largura, int jogador_altura) {
-    if (jogador_x + jogador_largura > x && jogador_x < x + LARGURA_TUBO) { // lado direito está depois do tubo mas o jogador/flappy está antes
-        if (jogador_y < altura_abertura || jogador_y + jogador_altura > altura_abertura + ESPACO_ENTRE_TUBOS) {
-            return true;// alt abertuda = borda inferior do tubo de cima; alt abertura + espaço é a borda inferior do de baixo
-        } // no caso vê se encosta ou pega no tubo inferior/superior
-    } // diz respeito a toda regiao do y, por isso nao precisa se preocupar tanto com batids laterais
+    if ((jogador_x + jogador_largura > this->x) &&                                       // o lado direito do jogador passou o lado esquerdo do tubo E
+        (jogador_x < this->x + LARGURA_TUBO) &&                                           // o lado esquerdo do jogador ainda não passou o lado direito do tubo E
+        (jogador_y + jogador_altura > this->altura_abertura - ALTURA_TUBO) &&  
+        (jogador_y < this->altura_abertura)) {                  
+        return true;
+    }
+    if ((jogador_x + jogador_largura > this->x) &&                                       // o lado direito do jogador passou o lado esquerdo do tubo E
+        (jogador_x < this->x + LARGURA_TUBO) &&                                           // o lado esquerdo do jogador ainda não passou o lado direito do tubo E
+        (jogador_y + jogador_altura > this->altura_abertura + ESPACO_ENTRE_TUBOS) &&
+        (jogador_y < this->altura_abertura + ESPACO_ENTRE_TUBOS + ALTURA_TUBO)) {  
+        return true;
+    }
+
     return false;
 }
